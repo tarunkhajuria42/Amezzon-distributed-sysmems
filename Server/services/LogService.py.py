a8d73fd58a1dd8f05d7b4effeeb5daffc4aafc91
tekraj chhetri <tekrajchhetri@gmail.com>
2018-09-24 17:45:23 +0300

    Helper file for log service -

---
+++ Server/services/LogService.py.py
@@ -0,0 +1,41 @@
+"""
+DEBUG	Detailed information, typically of interest only when diagnosing problems.
+INFO	Confirmation that things are working as expected.
+WARNING	An indication that something unexpected happened, or indicative of some problem in the near future (e.g. ‘disk space low’). The software is still working as expected.
+ERROR	Due to a more serious problem, the software has not been able to perform some function.
+CRITICAL	A serious error, indicating that the program itself may be unable to continue running.
+
+
+"""
+import logging
+
+def log(message, level):
+    logger = logging.getLogger(__name__)
+    logger.propagate = False
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    file_handler = logging.FileHandler('log31121.log')
+    file_handler.setFormatter(formatter)
+    logger.addHandler(file_handler)
+    if level == 0:
+        logger.setLevel(logging.DEBUG)
+        logger.debug(message)
+    if level == 1:
+        logger.setLevel(logging.INFO)
+        logger.info(message)
+    if level == 2:
+        logger.setLevel(logging.WARNING)
+        logger.warning(message)
+    if level == 3:
+        logger.setLevel(logging.ERROR)
+        logger.error(message)
+    if level == 4:
+        logger.setLevel(logging.CRITICAL)
+        logger.critical(message)
+    logger.removeHandler(file_handler)
+
+
+log('error',0 )
+log('not allowed to access',1 )
+log('not allowed to access server',2 )
+log('not allowed to access fatal error',3 )
+log('not allowed to access system crash',4 ) 
