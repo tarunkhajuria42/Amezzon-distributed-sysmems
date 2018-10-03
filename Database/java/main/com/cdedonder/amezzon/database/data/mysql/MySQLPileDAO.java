7696bad2745230e6a0bcf246851c02bc578c5322
cdedonder <cedric.dedonder@gmail.com>
2018-10-03 12:26:05 +0300

    pile dao

--- Database/java/main/com/cdedonder/amezzon/database/data/mysql/MySQLPileDAO.java
+++ Database/java/main/com/cdedonder/amezzon/database/data/mysql/MySQLPileDAO.java
@@ -5,27 +5,33 @@ import com.cdedonder.amezzon.database.DataAccessException;
 import com.cdedonder.amezzon.database.data.Pile;
 import com.cdedonder.amezzon.database.data.PileDAO;
 import com.cdedonder.amezzon.database.data.Product;
+import com.cdedonder.amezzon.database.data.ProductDAO;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
 
 @SuppressWarnings("Duplicates")
 public class MySQLPileDAO extends MySQLAbstractDAO implements PileDAO {
 
     private static final String CREATE_PILE = "INSERT INTO pile (pile_product, pile_sell, pile_buy) VALUES (?,?,?)";
     private static final String READ_PILE_BY_ID = "SELECT pile_id, pile_product, pile_sell, pile_buy FROM pile WHERE pile_id=?";
-    private static final String READ_ALL = "SELEXT pile_id, pile_product, pile_sell, pile_buy FROM pile";
+    private static final String READ_ALL = "SELECT pile_id, pile_product, pile_sell, pile_buy FROM pile";
     private static final String READ_PILE_BY_PRODUCT = "SELECT pile_id, pile_product, pile_sell, pile_buy FROM pile WHERE pile_product=?";
-    private static final String UPDATE_PILE = "UPDATE pile SET pile_product, pile_sell, pile_buy WHERE pile_id=?";
+    private static final String UPDATE_PILE = "UPDATE pile SET pile_product=?, pile_sell=?, pile_buy=? WHERE pile_id=?";
     private static final String DELETE_PILE = "DELETE FROM pile WHERE pile_id=?";
     private static final String DELETE_ALL = "DELETE FROM pile";
-    private static final String PILE_EXITS = "SELECT 1 FROM pile WHERE pile_id=?";
+    private static final String PILE_EXISTS = "SELECT 1 FROM pile WHERE pile_id=?";
+
+    private final ProductDAO productDAO;
 
     public MySQLPileDAO(Connection connection, DataAccesContext dac) {
         super(connection, dac);
+        this.productDAO = getDataAccesContext().getProductDAO();
     }
 
     private static void fillStatement(PreparedStatement stmt, Pile pile) throws SQLException {
@@ -38,7 +44,7 @@ public class MySQLPileDAO extends MySQLAbstractDAO implements PileDAO {
         pile.setId(set.getInt("pile_id"));
         pile.setSell(set.getDouble("pile_sell"));
         pile.setBuy(set.getDouble("pile_buy"));
-        pile.setProduct(getDataAccesContext().getProductDAO().read(set.getInt("pile_product")));
+        pile.setProduct(productDAO.read(set.getInt("pile_product")));
 
     }
 
@@ -80,32 +86,89 @@ public class MySQLPileDAO extends MySQLAbstractDAO implements PileDAO {
     }
 
     @Override
-    public Collection<Pile> readAll() {
-        return null;
+    public Collection<Pile> readAll() throws DataAccessException {
+        try {
+            try (PreparedStatement stmt = prepare(READ_ALL);
+                 ResultSet set = stmt.executeQuery()) {
+                List<Pile> pileList = new ArrayList<>();
+                while (set.next()) {
+                    Pile pile = new Pile();
+                    fillPile(set, pile);
+                    pileList.add(pile);
+                }
+                return pileList;
+            }
+        } catch (SQLException e) {
+            throw new DataAccessException(e);
+        }
     }
 
     @Override
-    public Pile readByProduct(Product product) {
-        return null;
+    public Pile readByProduct(Product product) throws DataAccessException {
+        try {
+            try (PreparedStatement stmt = prepare(READ_PILE_BY_PRODUCT)) {
+                stmt.setInt(1, product.getId());
+                try (ResultSet set = stmt.executeQuery()) {
+                    if (set.next()) {
+                        Pile pile = new Pile();
+                        fillPile(set, pile);
+                        return pile;
+                    }
+                    throw new DataAccessException("A pile with product " + product.getName() + " does not exist.");
+                }
+            }
+        } catch (SQLException e) {
+            throw new DataAccessException(e);
+        }
     }
 
     @Override
-    public void update(Pile pile) {
-
+    public void update(Pile pile) throws DataAccessException {
+        try {
+            try (PreparedStatement stmt = prepare(UPDATE_PILE)) {
+                fillStatement(stmt, pile);
+                stmt.setInt(4, pile.getId());
+                stmt.executeUpdate();
+            }
+        } catch (SQLException e) {
+            throw new DataAccessException(e);
+        }
     }
 
     @Override
-    public void delete(Pile pile) {
-
+    public void delete(Pile pile) throws DataAccessException {
+        try {
+            try (PreparedStatement stmt = prepare(DELETE_PILE)) {
+                stmt.setInt(1, pile.getId());
+                stmt.executeUpdate();
+            }
+        } catch (SQLException e) {
+            throw new DataAccessException(e);
+        }
     }
 
     @Override
-    public void deleteAll() {
-
+    public void deleteAll() throws DataAccessException {
+        try {
+            try (PreparedStatement stmt = prepare(DELETE_ALL)) {
+                stmt.executeUpdate();
+            }
+        } catch (SQLException e) {
+            throw new DataAccessException(e);
+        }
     }
 
     @Override
-    public boolean exists(Pile pile) {
-        return false;
+    public boolean exists(Pile pile) throws DataAccessException {
+        try {
+            try (PreparedStatement stmt = prepare(PILE_EXISTS)) {
+                stmt.setInt(1, pile.getId());
+                try (ResultSet set = stmt.executeQuery()) {
+                    return set.next();
+                }
+            }
+        } catch (SQLException e) {
+            throw new DataAccessException(e);
+        }
     }
 }
