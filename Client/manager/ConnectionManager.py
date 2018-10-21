7492de193b6377444abca31e322b80652ad4da3d
Aleksei <Aleksei.Kopolov@gmail.com>
2018-10-21 09:12:43 +0300

    Update

--- Client/manager/ConnectionManager.py
+++ Client/manager/ConnectionManager.py
@@ -1,5 +1,7 @@
-from httplib import HTTPSConnection
+import httplib
+import ssl
 
+CRL_PATH = 'crl/yourpemfile.pem'
 CONNECTION_DEFAULT = 'default'
 PORT = 'port'
 HOST = 'host'
@@ -7,12 +9,12 @@ HOST = 'host'
 
 class ConnectionManager(object):
     def __init__(self, service_manager):
-        self.connection_config = service_manager.get_connection_config()
-        self.connection_service = service_manager.get_connection_service()
+        self.con_config = dict(service_manager.get_connection_config())[CONNECTION_DEFAULT]
+        self.con_service = service_manager.get_connection_service()
         self.connection = None
 
     def set_connection(self):
-        conf = dict(self.connection_config._sections)[CONNECTION_DEFAULT]
-        self.connection = HTTPSConnection("{0}:{1}".format(conf[HOST], conf[PORT]))
-        self.connection.request('GET', '')
+        url = "{0}:{1}".format(self.con_config[HOST], self.con_config[PORT])
+        self.connection = httplib.HTTPConnection(url)
+        self.connection.request('GET', '/')
         print self.connection.getresponse()
