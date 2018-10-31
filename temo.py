970baf304ed35a503e0e80623701160e9b5ef217
Tarun Khajuria <tarunkhajuria42@gmail.com>
2018-10-31 18:55:54 +0200

    clean up

--- temo.py
+++
@@ -1,8 +0,0 @@
-import httplib
-import urllib
-import ssl
-conn=httplib.HTTPSConnection('localhost',8080,context=ssl._create_unverified_context())
-params = urllib.urlencode({'@number': 12524, '@type': 'issue', '@action': 'show'})
-headers = {"Content-type": "application/x-www-form-urlencoded",
-          "Accept": "text/plain"}
-conn.request("POST", "sd", params, headers)
