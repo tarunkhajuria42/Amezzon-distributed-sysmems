c2de1e72c619f4ba4973b19ee9f08dfc391c0e2a
cdedonder <cedric.dedonder@gmail.com>
2018-11-15 13:14:51 +0200

    filled in credentials

--- Database/java/src/test/java/testsuite/ConnectionTest.java
+++ Database/java/src/test/java/testsuite/ConnectionTest.java
@@ -1,4 +1,27 @@
 package testsuite;
 
+import com.cdedonder.amezzon.database.DataSourceFactory;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 public class ConnectionTest {
+
+    public static void main(String[] args) {
+        DataSource ds = DataSourceFactory.getMySQLDataSource();
+        try (Connection conn = ds.getConnection();
+             PreparedStatement ps = conn.prepareStatement("SHOW TABLES");
+             ResultSet set = ps.executeQuery()) {
+            while (set.next()) {
+                System.out.println(set.getString(1));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+
 }
