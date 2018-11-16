cf479062c9f3054c1eb2e7238a7ab194e1ca42a6
cdedonder <cedric.dedonder@gmail.com>
2018-11-16 12:13:55 +0200

    extend logging options

--- Database/java/src/main/java/com/cdedonder/amezzon/logging/DatabaseLogger.java
+++ Database/java/src/main/java/com/cdedonder/amezzon/logging/DatabaseLogger.java
@@ -1,25 +1,28 @@
 package com.cdedonder.amezzon.logging;
 
 import java.io.IOException;
-import java.util.logging.FileHandler;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-import java.util.logging.SimpleFormatter;
+import java.util.logging.*;
 
 public class DatabaseLogger {
 
     private static Logger LOGGER;
 
     public static void setup() throws IOException {
+        setup(Level.SEVERE, false);
+    }
+
+    public static void setup(Level level, boolean displayOnTerminal) throws IOException {
         LOGGER = Logger.getLogger(Logger.GLOBAL_LOGGER_NAME);
 
-        /*Logger rootLogger = Logger.getLogger("");
-        Handler[] handlers = rootLogger.getHandlers();
-        if(handlers[0] instanceof ConsoleHandler){
-            rootLogger.removeHandler(handlers[0]);
-        }*/
+        if (!displayOnTerminal) {
+            Logger rootLogger = Logger.getLogger("");
+            Handler[] handlers = rootLogger.getHandlers();
+            if (handlers[0] instanceof ConsoleHandler) {
+                rootLogger.removeHandler(handlers[0]);
+            }
+        }
 
-        LOGGER.setLevel(Level.INFO);
+        LOGGER.setLevel(level);
 
         FileHandler fileHandler = new FileHandler("dblog.txt");
         SimpleFormatter formatter = new SimpleFormatter();
