88ae2be0537912dcf2f211abc1ddba41a60cbe9d
Aleksei <Aleksei.Kopolov@gmail.com>
2018-12-04 16:51:24 +0200

    shell

---
+++ run.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+sudo docker start amezzon_db1
+sudo docker start amezzon_db2
+sudo docker start amezzon_db3
+sudo docker start amezzon_token
---
+++ stop.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+sudo docker stop amezzon_db1
+sudo docker stop amezzon_db2
+sudo docker stop amezzon_db3
+sudo docker stop amezzon_token
